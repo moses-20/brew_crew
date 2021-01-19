4bcad1a0bfd3ffcc4bffa7f4ce264e670f595dca
moses-20 <mosesgameli20@gmail.com>
2021-01-19 14:20:15 +0000

    complete brew_crew app

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/brew.model.dart';
+import 'package:brew_crew/models/user.model.dart';
 
 class DatabaseService {
   final String uid;
@@ -16,5 +18,35 @@ class DatabaseService {
     return await brewCollection
         .document(uid)
         .setData({'name': name, 'sugars': sugars, 'strength': strength});
+  }
+
+  // brew list form snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0');
+    }).toList();
+  }
+
+  // user data from snapshot
+  UserData _userDataFromSnapShot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+    );
+  }
+
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnapShot);
   }
 }
